a4fafd6491ed67c682b4dd02385f35ca1fa5c04c
tomikartemik <artemules@mail.ru>
2024-12-03 00:34:41 +0300

    echo bot

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+SESSION_NAME="shop"
+REPO_DIR="$HOME/tg_shop"
+GIT_REPO_URL="https://github.com/tomikartemik/tg_shop"
+
+echo "Starting deployment script"
+
+tmux_send() {
+    tmux send-keys -t $SESSION_NAME "$1" C-m
+}
+
+tmux kill-session -t $SESSION_NAME
+
+echo "Cloning/updating repository"
+if [ ! -d "$REPO_DIR" ]; then
+    git clone $GIT_REPO_URL $REPO_DIR
+fi
+
+cd $REPO_DIR && git pull origin main
+
+tmux new-session -d -s $SESSION_NAME
+tmux_send "go run main.go"
