c4baf0922e3eb4940bb40c7b933dece7da827c39
tomikartemik <artemules@mail.ru>
2024-12-14 22:46:56 +0300

    category ID -> id

--- internal/model/category.go
+++ internal/model/category.go
@@ -1,6 +1,6 @@
 package model
 
 type Category struct {
-	ID   int    `gorm:"autoIncrement;primaryKey" `
+	ID   int    `gorm:"autoIncrement;primaryKey" json:"id"`
 	Name string `gorm:"not null" json:"name"`
 }
