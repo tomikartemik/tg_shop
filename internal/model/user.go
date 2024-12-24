0e00e6973ad42c16dc5f155eca024fa3eb55e30d
tomikartemik <artemules@mail.ru>
2024-12-24 17:13:43 +0300

    change models

--- internal/model/user.go
+++ internal/model/user.go
@@ -3,15 +3,19 @@ package model
 import "time"
 
 type User struct {
-	TelegramID    int       `gorm:"primaryKey;uniqueIndex;not null" json:"telegram_id"`
-	Username      string    `gorm:"not null;default:'default_user'" json:"username"`
-	PhotoURL      string    `gorm:"not null;default:'uploads/default_hell_ava.jpg" json:"photo_url"`
-	IsAdmin       bool      `gorm:"not null;default:false" json:"is_admin"`
-	Balance       float64   `gorm:"default:0.0" json:"balance"`
-	Ads           []Ad      `gorm:"foreignKey:SellerID" json:"ads"`
-	Purchased     []Ad      `gorm:"many2many:user_purchased_ads" json:"purchased"`
-	Rating        float64   `gorm:"default:0.0" json:"rating"`
-	ReviewNumber  int       `gorm:"default:0" json:"review_number"`
+	TelegramID int     `gorm:"primaryKey;uniqueIndex;not null" json:"telegram_id"`
+	Username   string  `gorm:"not null;default:'default_user'" json:"username"`
+	PhotoURL   string  `gorm:"not null;default:'uploads/default_hell_ava.jpg" json:"photo_url"`
+	IsAdmin    bool    `gorm:"not null;default:false" json:"is_admin"`
+	Balance    float64 `gorm:"default:0.0" json:"balance"`
+	Ads        []Ad    `gorm:"foreignKey:SellerID" json:"ads"`
+	Purchased  []Ad    `gorm:"many2many:user_purchased_ads" json:"purchased"`
+	//PurchaseAmount float64   `gorm:"default:0.0" json:"purchase_amount"`
+	PurchaseAmount float64 `json:"purchase_amount"`
+	Rating         float64 `gorm:"default:0.0" json:"rating"`
+	ReviewNumber   int     `gorm:"default:0" json:"review_number"`
+	//SalesAmount    float64   `gorm:"default:0.0" json:"sales_amount"`
+	SalesAmount   float64   `json:"sales_amount"`
 	Banned        bool      `gorm:"default:false" json:"banned"`
 	IsPremium     bool      `gorm:"not null;default:false" json:"is_premium"`
 	ExpirePremium time.Time `gorm:"default:CURRENT_TIMESTAMP" json:"expire_premium"`
@@ -27,12 +31,14 @@ type UserAsSeller struct {
 }
 
 type UserInfo struct {
-	TelegramID   int           `json:"telegram_id"`
-	Username     string        `json:"username"`
-	PhotoURL     string        `json:"photo_url"`
-	Balance      float64       `json:"balance"`
-	Ads          []AdShortInfo `json:"ads"`
-	Purchased    []AdShortInfo `json:"purchased"`
-	Rating       float64       `json:"rating"`
-	ReviewNumber int           `json:"review_number"`
+	TelegramID     int           `json:"telegram_id"`
+	Username       string        `json:"username"`
+	PhotoURL       string        `json:"photo_url"`
+	Balance        float64       `json:"balance"`
+	Ads            []AdShortInfo `json:"ads"`
+	Purchased      []AdShortInfo `json:"purchased"`
+	PurchaseAmount float64       `json:"purchase_amount"`
+	Rating         float64       `json:"rating"`
+	ReviewNumber   int           `json:"review_number"`
+	SalesAmount    float64       `json:"sales_amount"`
 }
