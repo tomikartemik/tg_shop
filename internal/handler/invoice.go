faa0abaa7dea3d29a4c61ab61de278ba5fd61969
tomikartemik <artemules@mail.ru>
2025-01-29 03:26:02 +0300

    int -> string

--- internal/handler/invoice.go
+++ internal/handler/invoice.go
@@ -3,20 +3,32 @@ package handler
 import (
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"io/ioutil"
 	"net/http"
 	"tg_shop/internal/model"
 )
 
 func (h *Handler) paymentCallback(c *gin.Context) {
+	body, err := ioutil.ReadAll(c.Request.Body)
+	if err != nil {
+		newErrorResponse(c, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	// Выводим тело запроса в консоль
+	fmt.Println("Request Body:", string(body))
+
 	var paymentCallback model.PaymentCallback
 	if err := c.ShouldBindJSON(&paymentCallback); err != nil {
+		fmt.Println(err)
 		newErrorResponse(c, http.StatusBadRequest, err.Error())
 		return
 	}
 	fmt.Println(paymentCallback)
 
-	err := h.services.ChangeStatus(paymentCallback.OrderID, paymentCallback.Status)
+	err = h.services.ChangeStatus(paymentCallback.OrderID, paymentCallback.Status)
 	if err != nil {
+		fmt.Println(err)
 		newErrorResponse(c, http.StatusInternalServerError, err.Error())
 		return
 	}
